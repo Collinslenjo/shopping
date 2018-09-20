2f15fb107938a43a4eaaacefdd96339024809580
collinslenjo <collinslenjo.cl@gmail.com>
2018-09-20 13:01:26 +0300

    renamed project to src

--- shoppinglist/src/settings.py
+++ shoppinglist/src/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'shoppinglist.urls'
+ROOT_URLCONF = 'src.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'shoppinglist.wsgi.application'
+WSGI_APPLICATION = 'src.wsgi.application'
 
 
 # Database
